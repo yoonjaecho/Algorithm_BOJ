a3de7f49432a1ec8084a3f51dba265bd9c6653bb
yoon-jae <yoonjae.cho92@gmail.com>
2016-09-30 19:36:19 +0900

    https://www.acmicpc.net/problem/11502

--- problem/11502/11502.cpp
+++ problem/11502/11502.cpp
@@ -1,38 +1,22 @@
 #include <iostream>
 #include <cstdio>
-#include <cstring>
-#include <cstdlib>
 #include <cmath>
-#include <algorithm>
-#include <vector>
-#include <map>
-#include <queue>
-
-#define ll long long
-
 using namespace std;
-
 int isPrime(int n) {
-    for(int i=2; i<=sqrt(n); i++) {
+    for(int i=2; i<=sqrt(n); i++) 
         if(n%i == 0) return 0; 
-    }
     return 1;
 }
-
-int main()
-{
+int main() {
     int tc,n;
     scanf("%d",&tc);
-
     while(tc--) {
         bool found = false;
         scanf("%d",&n);
-
         if(isPrime(n-4)) {
             printf("2 2 %d\n",n-4);
             continue;
         } 
-
         for(int i=3; i<n; i++) {
             if(isPrime(i) && isPrime((n-i)/2)) {
                 printf("%d %d %d\n",i,(n-i)/2,(n-i)/2);
@@ -40,7 +24,6 @@ int main()
                 break;
             }
         }
-        if(!found)
-            puts("-1\n");
+        if(!found) puts("-1\n");
     }
 }
