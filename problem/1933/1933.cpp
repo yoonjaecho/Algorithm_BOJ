ba0751cb3e7eca555cd643164d524164011cbe1f
yoon-jae <yoonjae.cho92@gmail.com>
2016-11-09 12:36:47 +0900

    https://www.acmicpc.net/problem/1933

---
+++ problem/1933/1933.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+struct Event {
+    int x,h;
+    bool open;
+    Event() {}
+    Event(int xx, int hh, int oo) {
+        x = xx;
+        h = hh;
+        open = oo;
+    }
+};
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    vector<Event> ev;
+    while(n--) {
+        int l,h,r;
+        scanf("%d%d%d",&l,&h,&r);
+        ev.push_back(Event(l,h,true));
+        ev.push_back(Event(r,h,false));
+    }
+
+    sort(ev.begin(), ev.end(), [&](Event const &a, Event const &b) {
+        return a.x < b.x;
+    });
+
+    multiset<int> ms;
+    ms.insert(0);
+    int it = 0;
+    while(it != ev.size()) {
+        vector<Event> curEvent;
+        int cur_x = ev[it].x;
+        while(it < ev.size()) {
+            if(cur_x == ev[it].x)
+                curEvent.push_back(ev[it]);
+            else if(cur_x < ev[it].x) break;
+            it++;
+        }
+        int before = *ms.rbegin();
+        for(Event e : curEvent) {
+            if(e.open) ms.insert(e.h);
+            else ms.erase(ms.find(e.h));
+        }
+        int after = *ms.rbegin();
+        if(before != after)
+            printf("%d %d ", cur_x, after);
+    }
+}
