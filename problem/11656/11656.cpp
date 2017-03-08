ed7262392f969723b0898b87ff1cfe27c93cad44
yoon-jae <yoonjae.cho92@gmail.com>
2017-03-08 17:31:32 +0900

    https://www.acmicpc.net/problem/11656

--- problem/11656/11656.cpp
+++ problem/11656/11656.cpp
@@ -1,38 +1,13 @@
-#include <cstdio>
-#include <algorithm>
-#include <cstring>
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
-
-bool comparisonFunc(const char * c1, const char * c2)
-{
-	return strcmp(c1,c2) < 0;
-}
-
 int main() {
-
-	char input[1001];
-
-	vector<char*> arr;
-	vector<char*>::iterator it;
-
-	scanf("%s",input);
-
-	for(int i=0; i<=strlen(input)-1; i++) 
-		arr.push_back(input+i);
-
-	sort(arr.begin(),arr.end(),comparisonFunc);
-
-	
-	for(int i=0; i<=strlen(input)-1; i++) {
-		printf("%s\n",arr[i]);
-	}
-	
-
-
-	
-	
-
-	return 0;
+    string line; cin >> line;
+    vector<string> v;
+    for(int i=0; i<line.size(); i++)
+        v.push_back(line.substr(i,line.size()));
+    sort(v.begin(),v.end());
+    for(auto &x:v)
+        cout << x << endl;
 }
